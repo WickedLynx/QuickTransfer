6f7fb51f41189f4b7d055665100a7d47f7009948
Harshad <harshad@Harshads-Mac-mini.local>
2014-03-10 09:46:24 +0530

    Added documentation for Bonjour client and server delegate protocols

--- QuickTransfer/Classes/Networking/Bonjour/QTRBonjourClientDelegate.h
+++ QuickTransfer/Classes/Networking/Bonjour/QTRBonjourClientDelegate.h
@@ -12,12 +12,34 @@
 @class QTRUser;
 @class QTRFile;
 
+/*!
+ This protocol defines methods for file and connection related events of a Bonjour client.
+ */
 @protocol QTRBonjourClientDelegate <NSObject>
 
+/*!
+ @methodgroup Required methods
+ */
 @required
 
+/*!
+ The user who is currently usng the application.
+ */
 - (QTRUser *)localUser;
+
+/*!
+ When a client discovers a Bonjour server, it asks its delegate if it should connect to it.
+ 
+ The Bonjour client will always avoid connecting to a server instance belonging to the local user.
+
+ @param client The Bonjour client.
+ @param user The user who is running the server instance.
+ */
 - (BOOL)client:(QTRBonjourClient *)client shouldConnectToUser:(QTRUser *)user;
+
+/*!
+ The client asks its delegate for a file URL where a multipart file transfer must be written to
+ */
 - (NSURL *)saveURLForFile:(QTRFile *)file;
 
 @optional
--- QuickTransfer/Classes/Networking/Bonjour/QTRBonjourServerDelegate.h
+++ QuickTransfer/Classes/Networking/Bonjour/QTRBonjourServerDelegate.h
@@ -12,18 +12,59 @@
 @class QTRUser;
 @class QTRFile;
 
+/*!
+ This protocol contains forwards file and connection related events of the server to interested delegates.
+ */
 @protocol QTRBonjourServerDelegate <NSObject>
 
+/*!
+ @methodgroup Required methods
+ */
 @required
-
+/*!
+ The user who is using this app.
+ */
 - (QTRUser *)localUser;
+
+/*!
+ The server asks its delegate for a file URL to which an incoming large file must be written to.
+ @param file The file to be written.
+ */
 - (NSURL *)saveURLForFile:(QTRFile *)file;
 
+/*!
+ @methodgroup Optional methods
+ */
 @optional
 
+/*!
+ The server calls this method whenever it connects to a new user.
+ @param server The Bonjour server.
+ @param user The user which was connected.
+ */
 - (void)server:(QTRBonjourServer *)server didConnectToUser:(QTRUser *)user;
+
+/*!
+ The server calls this method whenever it disconnects a previously connected user.
+ @param server The Bonjour server.
+ @param user The user which was disconnected.
+ */
 - (void)server:(QTRBonjourServer *)server didDisconnectUser:(QTRUser *)user;
+
+/*!
+ The server calls this method after it receives a file from a user.
+ @param server The Bonjour server.
+ @param file The file that was received.
+ @param The user who sent the file.
+ */
 - (void)server:(QTRBonjourServer *)server didReceiveFile:(QTRFile *)file fromUser:(QTRUser *)user;
+
+/*!
+ The server calls this method when it has finished writing all parts of a multipart file transfer.
+ @param server The Bonjour server.
+ @param url The file URL where the file was saved.
+ @param user The user who sent the file.
+ */
 - (void)server:(QTRBonjourServer *)server didSaveReceivedFileAtURL:(NSURL *)url fromUser:(QTRUser *)user;
 
 
