e0173647718a6aa779c182b4cb0d985cd086b138
Tarun Yadav <tarun@leftshift.io>
2015-11-25 11:26:12 +0530

    Create home screen

---
+++ QuickTransfer-iOS/Classes/Views/QTRHomeCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  QTRHomeCollectionViewCell.h
+//  QuickTransfer
+//
+//  Created by Tarun Yadav on 24/11/15.
+//  Copyright © 2015 Laughing Buddha Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QTRHomeCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ QuickTransfer-iOS/Classes/Views/QTRHomeCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  QTRHomeCollectionViewCell.m
+//  QuickTransfer
+//
+//  Created by Tarun Yadav on 24/11/15.
+//  Copyright © 2015 Laughing Buddha Software. All rights reserved.
+//
+
+#import "QTRHomeCollectionViewCell.h"
+
+@implementation QTRHomeCollectionViewCell
+
+@end
