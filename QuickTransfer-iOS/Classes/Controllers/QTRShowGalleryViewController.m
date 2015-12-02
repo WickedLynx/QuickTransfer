c1b355b5b9acdace642c369d50e90320d41b1d5c
Tarun Yadav <tarun@leftshift.io>
2015-12-02 14:19:23 +0530

    Custom back button on galleryViewController

--- QuickTransfer-iOS/Classes/Controllers/QTRShowGalleryViewController.m
+++ QuickTransfer-iOS/Classes/Controllers/QTRShowGalleryViewController.m
@@ -46,10 +46,21 @@ int totalImages;
     [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRed:85.f/255.f green:85.f/255.f blue:85.f/255.f alpha:1.00f]];
     
     
-    UIBarButtonItem *leftBarButton = [[UIBarButtonItem alloc] initWithTitle:@"< Back" style:UIBarButtonItemStylePlain target:self action:@selector(homeButton)];
+    UIButton *leftCustomButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [leftCustomButton setImage:[UIImage imageNamed:@"back_arrow"] forState:UIControlStateNormal];
+    [leftCustomButton setTitle:@" Back" forState:UIControlStateNormal];
+    [leftCustomButton setTitleColor:[UIColor colorWithRed:32.f/255.f green:149.f/255.f blue:242.f/255.f alpha:1.00f] forState:UIControlStateNormal];
+    leftCustomButton.frame = CGRectMake(0.f, 0.f, 60.0f, 30.0f);
+    [leftCustomButton addTarget:self action:@selector(backButtonAction) forControlEvents:UIControlEventTouchUpInside];
+
     
-    [leftBarButton setTintColor:[UIColor colorWithRed:32.f/255.f green:149.f/255.f blue:242.f/255.f alpha:1.00f]];
-    [self.navigationItem setLeftBarButtonItem:leftBarButton];
+    UIBarButtonItem *leftBarButton = [[UIBarButtonItem alloc] init];
+    [leftBarButton setCustomView:leftCustomButton];
+    self.navigationItem.leftBarButtonItem=leftBarButton;
+    
+   
+//    [leftBarButton setTintColor:[UIColor colorWithRed:32.f/255.f green:149.f/255.f blue:242.f/255.f alpha:1.00f]];
+//    [self.navigationItem setLeftBarButtonItem:leftBarButton];
     
     QTRRightBarButtonView *customRightBarButton = [[QTRRightBarButtonView alloc]initWithFrame:CGRectZero];
     [customRightBarButton setUserInteractionEnabled:NO];
@@ -191,8 +202,12 @@ int totalImages;
         [self presentViewController:alertController animated:YES completion:nil];
     }
 
+}
 
- 
+-(void)backButtonAction {
+
+    [self.navigationController popToRootViewControllerAnimated:YES];
+
 }
 
 #pragma mark - UICollectionViewDataSource methods
