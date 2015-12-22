fe69fbc29442045748d28f1291b77cd9971f2472
harshad <harshad@leftshift.io>
2015-12-22 11:56:06 +0530

    QT-23 #time 30m Added a helper app to start QuickTransfer at login

---
+++ QuickTransferHelper/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  QuickTransferHelper
+//
+//  Created by Harshad on 22/12/15.
+//  Copyright © 2015 Laughing Buddha Software. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ QuickTransferHelper/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  QuickTransferHelper
+//
+//  Created by Harshad on 22/12/15.
+//  Copyright © 2015 Laughing Buddha Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
