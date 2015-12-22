c5541b0af5c192a21a7597a3c2e598a1546e1fdc
harshad <harshad@leftshift.io>
2015-12-22 12:31:05 +0530

    QT-23 #time 1h Enabled launch at login

--- QuickTransferHelper/AppDelegate.m
+++ QuickTransferHelper/AppDelegate.m
@@ -15,12 +15,13 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-}
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+// Source: http://stackoverflow.com/questions/11536477/mac-os-x-start-on-launch-while-in-app-store/11561143#11561143
+- (void)applicationWillFinishLaunching:(NSNotification *)notification {
+    NSString *appPath = [[[[[[NSBundle mainBundle] bundlePath] stringByDeletingLastPathComponent] stringByDeletingLastPathComponent] stringByDeletingLastPathComponent] stringByDeletingLastPathComponent];
+    // This string takes you from MyGreat.App/Contents/Library/LoginItems/MyHelper.app to MyGreat.App This is an obnoxious but dynamic way to do this since that specific Subpath is required
+    NSString *binaryPath = [[NSBundle bundleWithPath:appPath] executablePath]; // This gets the binary executable within your main application
+    [[NSWorkspace sharedWorkspace] launchApplication:binaryPath];
+    [NSApp terminate:nil];
 }
 
 @end
